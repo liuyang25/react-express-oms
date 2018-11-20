4b3a6513418f9e218eeefb01f94e35b869a269fb
liuyang <liuyang25@126.com>
2018-11-20 10:41:43 +0800

    时间处理

---
+++ backend/utils/common.js
@@ -0,0 +1,19 @@
+function dateFormat(date, fmt) {
+  if (null == date || undefined == date) return '';
+  var o = {
+      "M+": date.getMonth() + 1, //月份
+      "D+": date.getDate(), //日
+      "h+": date.getHours(), //小时
+      "m+": date.getMinutes(), //分
+      "s+": date.getSeconds(), //秒
+      "S": date.getMilliseconds() //毫秒
+  };
+  if (/(Y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+  for (var k in o)
+      if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+  return fmt;
+}
+
+module.exports = {
+  dateFormat
+}
